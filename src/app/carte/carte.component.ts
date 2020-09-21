eab05ab31e5663e8e4ffcc76c066867c35ed2747
Fabien <fabien.augros@ynov.com>
2020-09-21 12:04:45 +0200

    map2

--- src/app/carte/carte.component.ts
+++ src/app/carte/carte.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
@@ -6,17 +6,65 @@ import * as L from 'leaflet';
   templateUrl: './carte.component.html',
   styleUrls: ['./carte.component.scss']
 })
-export class CarteComponent implements OnInit {
+export class CarteComponent implements AfterViewInit {
+  map;
+
+  // retrieve from https://gist.github.com/ThomasG77/61fa02b35abf4b971390
+  smallIcon = new L.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon-2x.png',
+    iconSize:    [25, 41],
+    iconAnchor:  [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    shadowSize:  [41, 41]
+  });
 
   constructor() { }
 
-  ngOnInit() {
-    // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
-    const myfrugalmap = L.map('frugalmap').setView([50.6311634, 3.0599573], 12);
+  ngAfterViewInit(): void {
+    this.createMap();
+  }
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: 'Frugal Map'
-    }).addTo(myfrugalmap);
+  createMap() {
+    const parcThabor = {
+      lat: 48.114384,
+      lng: -1.669494,
+    };
+
+    const zoomLevel = 12;
+
+    this.map = L.map('map', {
+      center: [parcThabor.lat, parcThabor.lng],
+      zoom: zoomLevel
+    });
+
+    const mainLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      minZoom: 12,
+      maxZoom: 17,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    mainLayer.addTo(this.map);
+    const descriptionWikipedia = `
+      Le parc du Thabor, situé à Rennes à proximité du centre-ville,
+      est un parc public aménagé sur plus de dix hectares dont la particularité est de mêler un jardin à la française,
+      un jardin à l’anglaise et un important jardin botanique.`;
+    const popupOptions = {
+      coords: parcThabor,
+      text: descriptionWikipedia,
+      open: true
+    };
+    this.addMarker(popupOptions);
+  }
+
+  addMarker({coords, text, open}) {
+    const marker = L.marker([coords.lat, coords.lng], { icon: this.smallIcon });
+    if (open) {
+      marker.addTo(this.map).bindPopup(text).openPopup();
+    } else {
+      marker.addTo(this.map).bindPopup(text);
+    }
   }
 
 }
